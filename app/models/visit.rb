0c5f96116049340335ef1a9eea444e5334039d94
mamihery <mamihery@MC-G-LP-C120-4.local>
2016-09-29 15:00:53 +0200

    install gem annotate

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: visits
+#
+#  id         :integer          not null, primary key
+#  date_visit :datetime
+#  place_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Visit < ApplicationRecord
   belongs_to :place
 end
