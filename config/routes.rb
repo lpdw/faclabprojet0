0ab745f6493d4edd319ab03de61f6f1ad1e77be1
pierrecl <pc.clochard@gmail.com>
2016-09-29 16:10:15 +0200

    Fix erros in routes, sorry :(

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   resources :visits, :only => [:index,:show,:new,:destroy,:create]
-=======
+
   get 'stat' => 'stat#home'
 
   resources :visits, :only => [:index,:show,:new,:destroy]
->>>>>>> 1b98298364adf89d64fabbeb646509121227b52c
+
   resources :places
 end
