d2bf6ed6d386bad2496c1817ce3e0ddc313f48d4
mamihery <mamihery@MC-G-LP-C120-4.local>
2016-09-29 15:09:49 +0200

    annotate & graph

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'stat' => 'stat#home'
+
   resources :visits, :only => [:index,:show,:new,:destroy]
   resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
