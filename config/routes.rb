1c368a89bdc289ddbaa23f678d18b1061c2b36fb
mamihery <mamihery@MC-G-LP-C120-4.local>
2016-09-30 10:55:48 +0200

    root visit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'stat#home'
-  get 'stat' => 'stat#home'
+  root 'visit#show'
+  get 'visit' => 'stat#show'
   resources :visits, :only => [:index,:show,:new,:destroy,:create]
-  post 'stat' => 'stat#choicePlace'
   resources :places
 end
