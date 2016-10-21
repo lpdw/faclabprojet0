d160f2c3c2da626841e1ab129f29580fea476014
yann <kerfendalyann@gmail.com>
2016-10-21 12:37:44 +0200

    modifs intégration

---
+++ app/assets/javascripts/active.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function() {
+   	var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+   	$("nav li a").each(function(){
+        	if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+        	$(this).addClass("active");
+   	})
+
+
+
+});
